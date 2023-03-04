4c83ca9c5ce9d7cf166b383943b7b83f6ba9febd
ramesh <rameshjava4plus@gmail.com>
2023-03-05 01:04:12 +0530

    Initial commit

---
+++ src/main/java/com/mockitotutorial/happyhotel/booking/HappyController.java
@@ -0,0 +1,20 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HappyController {
+
+	private BookingService bookingService;
+	
+	@RequestMapping("/greeting")
+	public String index() {
+		return "Greetings from The Happy Hotel. We've got enough beds for " + bookingService.getAvailablePlaceCount() + " guests!";
+	}
+
+	public HappyController(BookingService bookingService) {
+		this.bookingService = bookingService;
+	}
+	
+}
